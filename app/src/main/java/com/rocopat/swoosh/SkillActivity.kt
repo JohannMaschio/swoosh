7a6b935db93951decc79f0e50cf41f981c928bb7
Rocopat <jbmaschio@gmail.com>
2018-02-13 14:27:50 -0200

    League page

--- app/src/main/java/com/rocopat/swoosh/SkillActivity.kt
+++ app/src/main/java/com/rocopat/swoosh/SkillActivity.kt
@@ -7,8 +7,11 @@ import android.view.View
 
 class SkillActivity : BaseActivity() {
 
+    var league = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
     }
 }

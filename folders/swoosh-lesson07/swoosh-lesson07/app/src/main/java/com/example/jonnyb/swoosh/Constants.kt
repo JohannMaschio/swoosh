f306316e30d5d368e8c9f80f3b67dad267e976ba
Rocopat <jbmaschio@gmail.com>
2018-02-12 22:09:43 -0200

    first page

---
+++ folders/swoosh-lesson07/swoosh-lesson07/app/src/main/java/com/example/jonnyb/swoosh/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.jonnyb.swoosh
+
+/**
+ * Created by jonnyb on 8/17/17.
+ */
+
+const val EXTRA_LEAGUE = "league"
---
+++ folders/swoosh-lesson08/swoosh-lesson08/app/src/main/java/com/example/jonnyb/swoosh/Utilities/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.jonnyb.swoosh.Utilities
+
+/**
+ * Created by jonnyb on 8/17/17.
+ */
+
+const val EXTRA_LEAGUE = "league"
+const val EXTRA_SKILL = "skill"
